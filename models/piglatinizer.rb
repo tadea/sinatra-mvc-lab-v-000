c59d6b1400693b7b1802ff4730b0f86618b40c93
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-08-22 15:15:02 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,22 @@
 class PigLatinizer
   attr_accessor :text
 
-  
-
+  def pig_to_latin(text)
+    @text = text.downcase
+    vowel = 'aeiou'
+    text = text.split()
+    text.map do |word|
+      if word.lenght == 1
+        "#{word}way"
+      elsif vowel.include? word [0].downcase
+        "#{word}way"
+      else 
+        split = word.split(/([aeiou])/,2)
+        "#{split[1]}#{split[2]}#{split[0]
+      end
+    end.join(' ')
+  end
+end
+        
 
 end
